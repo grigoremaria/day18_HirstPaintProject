abf33354d36e250759d6d5eea44786ea66bc43d9
grigoremaria <mariaagrigore30@gmail.com>
2025-02-10 14:18:02 +0200

    the hirst painting project

---
+++ the_hirst_painting_project.py
@@ -0,0 +1,45 @@
+import random
+import colorgram
+import turtle as turtle_module
+
+# use colorgram to extract the colours from the image and create a list with that colours
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 10)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+color_list = [(253, 251, 247), (253, 248, 252), (235, 252, 243), (198, 13, 32), (248, 236, 25), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49)]
+
+# create the painting with the colours from the list
+
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
